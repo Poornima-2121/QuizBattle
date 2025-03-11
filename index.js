0bc49d17ed3be14f046807d93d67e1c17eb48f1a
Poornima-2121 <poornimaidontcare@gmail.com>
2025-03-11 23:02:41 +0530

    Fetch Categories and Display them
    Fetching the categories for the URL and display it in the dropdown.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let player2Name = "";
 let categoryPage = document.querySelector(".categorySelectionSec");
 let startPage = document.querySelector(".startSec");
 let startGameBtn = document.getElementById("startGameBtn");
+let dropdown = document.getElementById("categoriesDropdown");
 let API = "https://the-trivia-api.com/v2/";
+let categories = []
 
 
 startGameBtn.addEventListener('click',startGame);
@@ -11,12 +13,27 @@ startGameBtn.addEventListener('click',startGame);
 function startGame(){
     player1Name = document.getElementById("player1name").value;
     player2Name = document.getElementById("player2name").value;
-    if(player1Name==="" && player2Name===""){
+    if(player1Name==="" || player2Name===""){
         alert("Enter Player Names");
     }
     else{
         startPage.style.display = "none";
         categoryPage.style.display = "block";
+        getcategories();
     }  
 }
 
+async function getcategories() {
+    let url = `${API}categories`;
+    let response = await fetch(url);
+    let data = await response.json();
+    categories = Object.keys(data)
+    displayCategories()
+}
+
+function displayCategories(){
+    dropdown.innerHTML = "";
+    categories.forEach((ele) => {
+        dropdown.innerHTML += `<option>${ele}</option>`;
+    })
+}
